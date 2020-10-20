f8e976dc6bc42db9f09ce669a09539318d0242ac
jshabree <jshabree@emich.edu>
2020-10-20 17:11:34 -0400

    rendering changes

--- server.js
+++ server.js
@@ -20,20 +20,13 @@ const db = require("./config/keys").MongodbURI;
 app.use(express.urlencoded({ extended: false }));
 
 
-// app.get("/users", (req, res, next) => {
-//   res.status(200).json({ Users: DummyUsers });
-// });
-
 app.get("/", (req, res, next) => {
     res.send("open")
 });
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
